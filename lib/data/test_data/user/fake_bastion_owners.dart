53f2053e85173c7ef984f8c1cb82cf0e43f67861
Barend Blom <barend@mathu.co.za>
2026-07-10 21:18:06 +0200

    feat: add fake bastion owners map to test data

---
+++ lib/data/test_data/user/fake_bastion_owners.dart
@@ -0,0 +1,6 @@
+const Map<String, String> bastionOwners = {
+  'bastion_2': 'Lord Gareth Thorne',
+  'bastion_3': 'Captain Mira Voss',
+  'bastion_4': 'Sage Elowen',
+  'bastion_5': 'Merchant Prince Aldrin',
+};
