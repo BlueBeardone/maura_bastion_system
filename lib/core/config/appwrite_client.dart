3de4c38f71541ad055aef953f495e9a1a932eb46
Barend Blom <barend@mathu.co.za>
2026-07-19 16:38:30 +0200

    Added appwrite stuff

---
+++ lib/core/config/appwrite_client.dart
@@ -0,0 +1,5 @@
+import 'package:appwrite/appwrite.dart';
+
+final Client client = Client()
+  .setProject("6a5bc196000fb1f857a6")
+  .setEndpoint("https://fra.cloud.appwrite.io/v1");
--- lib/features/main_menu/main_page.dart
+++ lib/features/main_menu/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maura_bastion_system/core/config/appwrite_client.dart';
 import 'package:maura_bastion_system/features/news_paper/presentation/news_paper.dart';
 import 'package:maura_bastion_system/widgets/standard_scaffold/standard_scaffold.dart';
 
@@ -8,7 +9,18 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StandardScaffold(
-      body: NewspaperLayout(),
+      body: Column(
+        children: [
+          Expanded(child: NewspaperLayout()),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: () => client.ping(),
+              child: const Text('Send a ping'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
