0d31a8790050ed3a79e3b5086f21c494ee8c8a60
Barend Blom <barend@mathu.co.za>
2026-07-20 06:47:37 +0200

    Removed app write

--- lib/features/main_menu/main_page.dart
+++ lib/features/main_menu/main_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:maura_bastion_system/core/config/appwrite_client.dart';
 import 'package:maura_bastion_system/features/news_paper/presentation/news_paper.dart';
 import 'package:maura_bastion_system/widgets/standard_scaffold/standard_scaffold.dart';
 
@@ -9,18 +8,7 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StandardScaffold(
-      body: Column(
-        children: [
-          Expanded(child: NewspaperLayout()),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () => client.ping(),
-              child: const Text('Send a ping'),
-            ),
-          ),
-        ],
-      ),
+      body: NewspaperLayout(),
     );
   }
 }
